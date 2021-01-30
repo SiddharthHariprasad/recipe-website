648e82de67b851432ac9522d6a8512b994e7b6e0
Siddharth Hariprasad <siddhuhariprasad@gmail.com>
2021-01-30 20:11:32 +0530

    Adding the single recipe page

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ $(document).ready(function(){
     var r = {};
 
     var homeHtml = "snippets/home.html";
-    var cakesHtml = "snippets/cakes.html";
+    var cakesHtml = "snippets/cakesIndex.html";
     var cakesUrl = "json/cakes.json";
+    var singleCakeHtml = "snippets/singleCake.html";
 
     // Convenience function for inserting innerHTML for 'select'
   	var insertHtml = function (selector, html) {
@@ -46,7 +47,7 @@ $(document).ready(function(){
     });
     
     // Load Cakes Index
-	r.loadCakes = function () {
+	r.loadCakesIndex = function () {
 		// showLoading("#main-content");
 		$ajaxUtils.sendGetRequest(cakesUrl,buildAndShowCakesHTML);
     };
@@ -65,7 +66,8 @@ $(document).ready(function(){
     
     // Using cakes data and snippets html build cakes view HTML to be inserted into page
 	function buildCakesViewHtml(cakes,cakesHtml) {
-        var finalHTML = "<h2>Cakes</h2>";
+        var finalHTML = "";
+        finalHTML += "<h2>Cakes</h2>";
         
 
 		// Loop over cakes
@@ -73,7 +75,7 @@ $(document).ready(function(){
 			// insert cakes values
 			var html = cakesHtml;
 			var cake_id = cakes[i].cake_id;
-			var cake_image = cakes[i].cakes_image;
+			// var cake_image = cakes[i].cakes_image;
             var cake_alt = cakes[i].cake_alt;
             var cake_title = cakes[i].cake_title;
             var cake_desc = cakes[i].cake_desc;
@@ -82,6 +84,53 @@ $(document).ready(function(){
             html = insertProperty(html,"cake_alt",cake_alt);
             html = insertProperty(html, "cake_title",cake_title);
             html = insertProperty(html,"cake_desc",cake_desc);
+			finalHTML += html;
+		}
+		return finalHTML;
+    }
+    
+    // Load Cake Recipie
+	r.loadCake = function () {
+		// showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(cakesUrl,buildAndShowSingleCakeHTML);
+    };
+
+    // Builds HTML for the single Cake page based on the data from the server
+	function buildAndShowSingleCakesHTML(cakes) {
+		// Retrive Articles Index Snippet
+		$ajaxUtils.sendGetRequest(
+          singleCakeHtml,
+          function (singleCakeHtml) {
+            var singleCakeViewHtml = buildSingleCakeViewHtml(cakes,singleCakeHtml);
+            insertHtml("#main-content", singleCakeViewHtml);
+          },
+          false);
+    }
+    
+    // Using single cake data and snippets html build cakes view HTML to be inserted into page
+	function buildSingleCakeViewHtml(cakes,singleCakeHtml) {
+        var finalHTML = "";
+        finalHTML += "<h2>Cakes</h2>";
+        
+
+		// Loop over cakes
+		for (var i = 0; i < cakes.length; i++) {
+			// insert cakes values
+			var html = cakesHtml;
+			var cake_id = cakes[i].cake_id;
+			// var cake_image = cakes[i].cakes_image;
+            var cake_alt = cakes[i].cake_alt;
+            var cake_title = cakes[i].cake_title;
+            var cake_desc = cakes[i].cake_desc;
+            var cake_ingredients = cakes[i].cake_ingredients;
+            var cake_recipe = cakes[i].cake_recipe;
+			html = insertProperty(html,"cake_id",cake_id);
+			// html = insertProperty(html,"cake_image",cake_image);
+            html = insertProperty(html,"cake_alt",cake_alt);
+            html = insertProperty(html, "cake_title",cake_title);
+            html = insertProperty(html,"cake_desc",cake_desc);
+            html = insertProperty(html,"cake_ingredients",cake_ingredients);
+            html = insertProperty(html,"cake_recipe",cake_recipe);
 			finalHTML += html;
 		}
 		return finalHTML;
