3eafefa558295797cba5221f56d3ecf90dbd7b46
Siddharth Hariprasad <siddhuhariprasad@gmail.com>
2021-01-30 19:14:55 +0530

    fixing dynamic loading

--- js/script.js
+++ js/script.js
@@ -36,14 +36,15 @@ $(document).ready(function(){
     // On page load (before images or CSS)
 	document.addEventListener("DOMContentLoaded", function (event) {
 
-		// On first load, show home view
-		// showLoading("#main-content");
-		$ajaxUtils.sendGetRequest(
-			homeHtml, 
-			function (responseText) {
-				document.querySelector("#main-content").innerHTML = responseText;
-			},
-        false);
+		// // On first load, show home view
+		// // showLoading("#main-content");
+		// $ajaxUtils.sendGetRequest(
+		// 	homeHtml, 
+		// 	function (responseText) {
+		// 		document.querySelector("#main-content").innerHTML = responseText;
+		// 	},
+        // false);
+        r.loadCakes();
     });
     
     // Load Cakes Index
