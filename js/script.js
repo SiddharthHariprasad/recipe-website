4156261912d9537c9a27af7bcf2ee96df9ed7c76
Siddharth Hariprasad <siddhuhariprasad@gmail.com>
2021-01-30 20:20:20 +0530

    Fixing single Recipe Page

--- js/script.js
+++ js/script.js
@@ -90,13 +90,14 @@ $(document).ready(function(){
     }
     
     // Load Cake Recipie
-	r.loadCake = function () {
-		// showLoading("#main-content");
+	r.loadCake = function (cID) {
+        // showLoading("#main-content");
+        cakeID = cID;
 		$ajaxUtils.sendGetRequest(cakesUrl,buildAndShowSingleCakeHTML);
     };
 
     // Builds HTML for the single Cake page based on the data from the server
-	function buildAndShowSingleCakesHTML(cakes) {
+	function buildAndShowSingleCakeHTML (cakes) {
 		// Retrive Articles Index Snippet
 		$ajaxUtils.sendGetRequest(
           singleCakeHtml,
@@ -110,28 +111,28 @@ $(document).ready(function(){
     // Using single cake data and snippets html build cakes view HTML to be inserted into page
 	function buildSingleCakeViewHtml(cakes,singleCakeHtml) {
         var finalHTML = "";
-        finalHTML += "<h2>Cakes</h2>";
         
-
 		// Loop over cakes
 		for (var i = 0; i < cakes.length; i++) {
-			// insert cakes values
-			var html = cakesHtml;
-			var cake_id = cakes[i].cake_id;
-			// var cake_image = cakes[i].cakes_image;
-            var cake_alt = cakes[i].cake_alt;
-            var cake_title = cakes[i].cake_title;
-            var cake_desc = cakes[i].cake_desc;
-            var cake_ingredients = cakes[i].cake_ingredients;
-            var cake_recipe = cakes[i].cake_recipe;
-			html = insertProperty(html,"cake_id",cake_id);
-			// html = insertProperty(html,"cake_image",cake_image);
-            html = insertProperty(html,"cake_alt",cake_alt);
-            html = insertProperty(html, "cake_title",cake_title);
-            html = insertProperty(html,"cake_desc",cake_desc);
-            html = insertProperty(html,"cake_ingredients",cake_ingredients);
-            html = insertProperty(html,"cake_recipe",cake_recipe);
-			finalHTML += html;
+            if(cakes[i].cake_id==cakeID){
+                // insert cakes values
+                var html = cakesHtml;
+                var cake_id = cakes[i].cake_id;
+                // var cake_image = cakes[i].cakes_image;
+                var cake_alt = cakes[i].cake_alt;
+                var cake_title = cakes[i].cake_title;
+                var cake_desc = cakes[i].cake_desc;
+                var cake_ingredients = cakes[i].cake_ingredients;
+                var cake_recipe = cakes[i].cake_recipe;
+                html = insertProperty(html,"cake_id",cake_id);
+                // html = insertProperty(html,"cake_image",cake_image);
+                html = insertProperty(html,"cake_alt",cake_alt);
+                html = insertProperty(html, "cake_title",cake_title);
+                html = insertProperty(html,"cake_desc",cake_desc);
+                html = insertProperty(html,"cake_ingredients",cake_ingredients);
+                html = insertProperty(html,"cake_recipe",cake_recipe);
+                finalHTML += html;
+            }
 		}
 		return finalHTML;
 	}
