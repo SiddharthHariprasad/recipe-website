984e17e89ef4b15d72da47bf86894563970a85df
Siddharth Hariprasad <siddhuhariprasad@gmail.com>
2021-01-30 23:21:38 +0530

    added currys page

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ $(document).ready(function(){
     var cakesHtml = "snippets/cakesIndex.html";
     var cakesUrl = "json/cakes.json";
     var singleCakeHtml = "snippets/singleCake.html";
+    var currysHtml = "snippets/currysIndex.html";
+    var currysUrl = "json/currys.json";
+    var singleCurryHtml = "snippets/singleCurry.html";
 
     // Convenience function for inserting innerHTML for 'select'
   	var insertHtml = function (selector, html) {
@@ -54,7 +57,7 @@ $(document).ready(function(){
 
     // Builds HTML for the Cakes Index page based on the data from the server
 	function buildAndShowCakesHTML(cakes) {
-		// Retrive Articles Index Snippet
+		// Retrive Cakes Index Snippet
 		$ajaxUtils.sendGetRequest(
           cakesHtml,
           function (cakesHtml) {
@@ -94,7 +97,7 @@ $(document).ready(function(){
 
     // Builds HTML for the single Cake page based on the data from the server
 	function buildAndShowSingleCakeHTML (cakes) {
-		// Retrive Articles Index Snippet
+		// Retrive Single Cake Snippet
 		$ajaxUtils.sendGetRequest(
           singleCakeHtml,
           function (singleCakeHtml) {
@@ -123,6 +126,89 @@ $(document).ready(function(){
                 html = insertProperty(html,"cake_desc",cake_desc);
                 html = insertProperty(html,"cake_ingredients",cake_ingredients);
                 html = insertProperty(html,"cake_recipe",cake_recipe);
+                finalHTML += html;
+            }
+		}
+		return finalHTML;
+    }
+    
+    // Load Currys Index
+	r.loadCurrysIndex = function () {
+		// showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(currysUrl,buildAndShowCurrysHTML);
+    };
+
+    // Builds HTML for the Currys Index page based on the data from the server
+	function buildAndShowCurrysHTML(currys) {
+		// Retrive Currys Index Snippet
+		$ajaxUtils.sendGetRequest(
+          currysHtml,
+          function (currysHtml) {
+            var currysViewHtml = buildCurrysViewHtml(currys,currysHtml);
+            insertHtml("#main-content", currysViewHtml);
+          },
+          false);
+    }
+    
+    // Using currys data and snippets html build currys view HTML to be inserted into page
+	function buildCurrysViewHtml(currys,currysHtml) {
+        var finalHTML = "";
+        finalHTML += "<h2>Currys</h2>";
+        
+
+		// Loop over currys
+		for (var i = 0; i < currys.length; i++) {
+			// insert currys values
+			var html = currysHtml;
+			var curry_id = currys[i].curry_id;
+            var curry_title = currys[i].curry_title;
+            var curry_desc = currys[i].curry_desc;
+			html = insertProperty(html,"curry_id",curry_id);
+            html = insertProperty(html, "curry_title",curry_title);
+            html = insertProperty(html,"curry_desc",curry_desc);
+			finalHTML += html;
+		}
+		return finalHTML;
+    }
+    
+    // Load curry Recipie
+	r.loadcurry = function (cID) {
+        // showLoading("#main-content");
+        curryID = cID;
+		$ajaxUtils.sendGetRequest(currysUrl,buildAndShowSingleCurryHTML);
+    };
+
+    // Builds HTML for the single Curry page based on the data from the server
+	function buildAndShowSingleCurryHTML (currys) {
+		// Retrive Articles Index Snippet
+		$ajaxUtils.sendGetRequest(
+          singleCurryHtml,
+          function (singleCurryHtml) {
+            var singleCurryViewHtml = buildSingleCurryViewHtml(currys,singleCurryHtml);
+            insertHtml("#main-content", singleCurryViewHtml);
+          },
+          false);
+    }
+    
+    // Using single Curry data and snippets html build Currys view HTML to be inserted into page
+	function buildSingleCurryViewHtml(currys,singleCurryHtml) {
+        var finalHTML = "";
+        
+		// Loop over Currys
+		for (var i = 0; i < currys.length; i++) {
+            if(currys[i].curry_id==curryID){
+                // insert curry values
+                var html = singlecurryHtml;
+                var curry_id = currys[i].curry_id;
+                var curry_title = currys[i].curry_title;
+                var curry_desc = currys[i].curry_desc;
+                var curry_ingredients = currys[i].curry_ingredients;
+                var curry_recipe = currys[i].curry_recipe;
+                html = insertProperty(html,"curry_id",curry_id);
+                html = insertProperty(html, "curry_title",curry_title);
+                html = insertProperty(html,"curry_desc",curry_desc);
+                html = insertProperty(html,"curry_ingredients",curry_ingredients);
+                html = insertProperty(html,"curry_recipe",curry_recipe);
                 finalHTML += html;
             }
 		}
